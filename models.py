8ae9396fd7ac56b1dcfd7bbc9cdabaabfbba6ca6
MY-HERO-OP <129320317+MY-HERO-OP@users.noreply.github.com>
2024-06-25 00:36:49 +0530

    Update models.py

--- models.py
+++ models.py
@@ -8,6 +8,7 @@ models = os.getenv("MODELS")
 MODELS = [
    "prompthero/openjourney",
    "digiplay/AbsoluteReality_v1.8.1",
+   "phenixrhyder/NSFW-generator",
    "digiplay/MengX_Mix_Fantasy_v4",
    "Nymbo/NSFW-generator",
    "stabilityai/stable-diffusion-xl-base-1.0",
